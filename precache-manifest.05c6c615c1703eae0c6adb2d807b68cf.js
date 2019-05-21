34f972cb5fa3d0a091914aa9677a8cb72410162c
zeevgg <zeevgg@gmail.com>
2019-05-21 19:35:52 +0300

    Updates

---
+++ precache-manifest.05c6c615c1703eae0c6adb2d807b68cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf013e4449a3c53b076e488e6eeb84c",
+    "url": "/movieApp_react/index.html"
+  },
+  {
+    "revision": "405f1a9df5c2146bacb3",
+    "url": "/movieApp_react/static/css/main.6370cbe8.chunk.css"
+  },
+  {
+    "revision": "172a51d5722197eacad2",
+    "url": "/movieApp_react/static/js/2.6c7f1c22.chunk.js"
+  },
+  {
+    "revision": "405f1a9df5c2146bacb3",
+    "url": "/movieApp_react/static/js/main.617c7dc9.chunk.js"
+  },
+  {
+    "revision": "96459426e7d5f453e04b",
+    "url": "/movieApp_react/static/js/runtime~main.86024f93.js"
+  },
+  {
+    "revision": "84a9d30d67a43675723449c648ffae5a",
+    "url": "/movieApp_react/static/media/cursor.84a9d30d.svg"
+  }
+]);
